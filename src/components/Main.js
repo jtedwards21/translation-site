d87e2191d05c43df09a2f4aec36cc251dbfbf6b9
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-01-22 23:24:09 +0800

    main text

--- src/components/Main.js
+++ src/components/Main.js
@@ -19,22 +19,26 @@ class Main extends React.Component {
           <p><strong>Do you have a global business? Are you competing within international markets with multiple languages and platforms?</strong></p>
           <p>If the answer is yes, then localization of your website, mobile app, videogame or software should be a vital component of your marketing strategy. It’s essential to not deliver an averagely-translated marketing message that your prospects merely understand; you want to speak to them in their own language and create a message that truly resonates with them.</p>
           <p>Localization and translation services complement one another and ensure your brand message hits its mark. Localization optimizes your content by using precise, targeted keywords that will improve your search engine ranking and conversion rate. Trust your global growth and success with one of the most reputable translation and localization companies in the industry</p>
-
           {close}
         </article>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
           <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <p>We have experience in the following areas. PDF work samples are provided for your convenience.</p>
+          <p><strong>Technical: </strong><a href="#">Sample</a><strong><br/>Marketing: </strong><a href="#">Sample</a><strong><br/>SEO and SEM: </strong><a href="#">Sample</a><strong><br/>Online Retailers: </strong><a href="#">Sample</a></p>
           {close}
         </article>
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <p>My name is Joshua. I was born in the United States and am fluent in English and Mandarin Chinese. </p>
+          <p>I have a degree in computer science from the University of North Carolina and have worked in the technical and business sectors in China and the United States for several years.</p>
+          <p>I translate from Mandarin Chinese to English. The majority of my experience is in website and text translation (various topics). Keeping deadlines is a top priority for me.</p>
+          <p>Some are the topics I specialize in are:</p>
+          <p><strong>Technical<br/>Marketing<br/>SEO and SEM<br/>Online Retailers</strong></p>
+          <p>As an English native speaker I guarantee that my translations will be correct and that you will be satisfied. Feel free to contact me any time!</p>
           {close}
         </article>
 
