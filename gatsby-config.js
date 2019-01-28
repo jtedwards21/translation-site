d35cb5a1b4f28b21501654303adc5d8366f828c4
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-01-28 12:32:42 +0800

    changed tab title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "SCRIPTT - Translation, Localization and Marketing",
+    author: "Joshua Edwards",
+    description: "A translation, localization and marketing website"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
