df536d966de2f31efec1216cf1937bc085288db8
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-01-29 22:56:45 +0800

    fixed contact information

--- src/components/Main.js
+++ src/components/Main.js
@@ -45,24 +45,14 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
+              <dl className="alt">
+                  <dt>Address</dt>
+                  <dd>Kangqiao Gardens &bull; Jiaxing, Zhejiang 314000 &bull; China</dd>
+                  <dt>Phone</dt>
+                  <dd>(+86) 159 58377430</dd>
+                  <dt>Email</dt>
+                  <dd><a href="#">zhu.lingli@scriptt.me</a></dd>
+              </dl>
           {close}
         </article>
 
